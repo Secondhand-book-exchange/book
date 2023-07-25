46621ae9e5d50559638e7b28a182431325a8ad8a
entellaKa <inchol321@gmail.com>
2023-07-25 17:09:17 +0900

    MainActivity initial setting

--- app/src/main/java/com/sangwon/example/bookapp/MainActivity.kt
+++ app/src/main/java/com/sangwon/example/bookapp/MainActivity.kt
@@ -1,13 +1,38 @@
 package com.sangwon.example.bookapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.SpinnerAdapter
 import com.google.firebase.FirebaseApp
+import com.sangwon.example.bookapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener, AdapterView.OnItemClickListener {
+    private val binding = ActivityMainBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         FirebaseApp.initializeApp(this)
+
+        binding.searchBtn.setOnClickListener(this)
+        binding.areaBtn.setOnClickListener(this)
+        binding.bookList.setOnItemClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            binding.searchBtn.id->
+                startActivity(Intent(this, SearchActivity::class.java))
+            binding.areaBtn.id->
+                startActivity(Intent(this, SelectAreaActivity::class.java))
+        }
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val intent = Intent(this, BookInfo::class.java)
+        intent.putExtra("name", "bookName")
+        startActivity(intent)
     }
 }
