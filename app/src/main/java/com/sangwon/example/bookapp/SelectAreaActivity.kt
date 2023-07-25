303964e87e9646f41f2773003f110baab0c46516
entellaKa <inchol321@gmail.com>
2023-07-25 18:50:09 +0900

    Insert new Activity
    필요할거 같아서 브랜치 나눴는데 굳이 필요 없으려나

---
+++ app/src/main/java/com/sangwon/example/bookapp/SelectAreaActivity.kt
@@ -0,0 +1,11 @@
+package com.sangwon.example.bookapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectAreaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_area)
+    }
+}
