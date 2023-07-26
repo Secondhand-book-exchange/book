3039d00fe1736660cd202c81408fb02eb9dfbe3a
Sangwon <qmfdnjssk@tukorea.ac.kr>
2023-07-26 12:20:14 +0900

    feat: db재료 2

--- app/src/main/java/com/sangwon/example/bookapp/MainActivity.kt
+++ app/src/main/java/com/sangwon/example/bookapp/MainActivity.kt
@@ -3,11 +3,16 @@ package com.sangwon.example.bookapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.FirebaseApp
+import com.sangwon.example.bookapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        FirebaseApp.initializeApp(this)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        view.button
+
     }
 }
--- app/src/main/java/com/sangwon/example/bookapp/data.kt
+++ app/src/main/java/com/sangwon/example/bookapp/data.kt
@@ -1,2 +1,22 @@
 package com.sangwon.example.bookapp
 
+data class Posts(
+    val BookTitle: String? = null,
+    val Author:String? = null,
+    val Locate: String? = null,
+
+    val Subscript: String? = null,
+    val Image: String? = null, //이거 어캐하냐?
+
+    val Uid: String? = null,
+    val Time: String? = null, //등록 시간
+
+)
+class Model {
+    var imageUrl: String? = null
+
+    internal constructor() {}
+    constructor(imageUrl: String?) {
+        this.imageUrl = imageUrl
+    }
+}
