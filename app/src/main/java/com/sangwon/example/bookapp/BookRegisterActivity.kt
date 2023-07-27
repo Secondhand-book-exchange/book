f678c6a933c925dca8c581d0cc4ec4ae96d16474
Sangwon <qmfdnjssk@tukorea.ac.kr>
2023-07-27 13:55:26 +0900

    feat : 파이어스토리지에 이미지 저장

--- app/src/main/java/com/sangwon/example/bookapp/BookRegisterActivity.kt
+++ app/src/main/java/com/sangwon/example/bookapp/BookRegisterActivity.kt
@@ -5,53 +5,61 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.webkit.MimeTypeMap
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import com.sangwon.example.bookapp.databinding.ActivityBookRegisterBinding
 import com.sangwon.example.bookapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import kotlin.properties.Delegates
 
 class BookRegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBookRegisterBinding
     val db = Firebase.firestore
     private lateinit var imageUri: Uri
+    val auth = Firebase.auth
+    private lateinit var BookTitle  : String
+    private lateinit var Author : String
+    private lateinit var Location : String
+    private lateinit var BookStatus : String
+    private lateinit var Subscript : String
+    private var IsSale by Delegates.notNull<Boolean>()
+
+
+    val user = auth.currentUser
+    var imagePath: String = " "
+
 
     var storage = Firebase.storage
-    var storageReference = storage.reference
+    var storageRef = storage.reference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBookRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.RegisterBook.setOnClickListener {
-            val BookTitle = binding.BookTitle.text.toString()
-            val Author = binding.Author.text.toString()
-            val Location = binding.Location.text.toString()
-            val BookStatus = binding.BookStatus.toString()
-            val Subscript = binding.Subscript.text.toString()
-            val IsSale = false
+            BookTitle = binding.BookTitle.text.toString()
+            Author = binding.Author.text.toString()
+            Location = binding.Location.text.toString()
+            BookStatus = binding.BookStatus.toString()
+            Subscript = binding.Subscript.text.toString()
+            IsSale = false
 
-            val Post = Posts(
-                BookTitle,
-                Author,
-                Location,
-                BookStatus,
-                Subscript,
-                null,
-                IsSale,
-                null,
-                null,
-            )
 
-            db.collection("Posts")
-                .add(Post)
-                .addOnSuccessListener { documentReference ->
-                    Log.d("db", "DocumentSnapshot added with ID: ${documentReference.id}")
-                }
-                .addOnFailureListener { e ->
-                    Log.w("db", "Error adding document", e)
-                }
-            finish()
+            uploadToFirestore(imageUri) //아무 이미지도 안넣으면 어떻게 되냐? 비동기라서 밑에 imagePath에 값 들어가기 전에 등록되는거 아니야?
+
+
+
+
+
         }
 
         binding.BookImage.setOnClickListener {
@@ -60,11 +68,6 @@ class BookRegisterActivity : AppCompatActivity() {
             galleryIntent.type = "image/*"
             startActivityForResult(galleryIntent, 1)
         }
-
-
-
-
-
 
 
 
@@ -82,8 +85,73 @@ class BookRegisterActivity : AppCompatActivity() {
 
 
             // Uri 이미지 뷰에 넣으면 사진 나와
+            binding.BookImage.setBackgroundResource(0)
             binding.BookImage.setImageURI(imageUri)
 
         }
     }
+    // Upload the image to Firestore
+    private fun uploadToFirestore(imageUri: Uri) {
+        val imageName = System.currentTimeMillis().toString() + "." + getFileExtension(imageUri)
+        val fileref = storageRef.child("images/$imageName")
+
+        // 업로드를 코루틴으로 처리
+        lifecycleScope.launch {
+            try {
+                // 파일 업로드 비동기 작업을 완료할 때까지 대기
+                val uploadTask = withContext(Dispatchers.IO) {
+                    fileref.putFile(imageUri).await()
+                }
+
+                // 업로드가 성공적으로 완료되면 이미지 경로 설정
+                imagePath = "images/$imageName"
+
+                // 이제 여기서 Post 객체를 생성하고 데이터베이스에 추가할 수 있습니다.
+                val Post = Posts(
+                    BookTitle,
+                    Author,
+                    Location,
+                    BookStatus,
+                    Subscript,
+                    imagePath,
+                    IsSale,
+                    user!!.uid,
+                    null,
+                )
+
+                // 데이터베이스에 추가하는 코드는 여기에 작성하면 됩니다.
+                db.collection("Posts")
+                    .add(Post)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d("db", "DocumentSnapshot added with ID: ${documentReference.id}")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("db", "Error adding document", e)
+                    }
+
+                Toast.makeText(this@BookRegisterActivity,"업로드 성공",Toast.LENGTH_SHORT).show()
+                // 작업 완료
+                finish()
+            } catch (e: Exception) {
+                // 업로드 실패 처리
+                Toast.makeText(this@BookRegisterActivity, "업로드 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getFileExtension(imageUri: Uri): String {
+
+        // Get the content resolver
+        val contentResolver = contentResolver
+
+        // Get the mime type
+        val mimeType = contentResolver.getType(imageUri)
+
+        // Get the file extension
+        val extension = MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType)
+
+        return extension.toString()
+    }
+
+
 }
