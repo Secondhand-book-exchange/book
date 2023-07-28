2649c7a31fcb3f7246809b84b649d4f558b431fc
entellaKa <inchol321@gmail.com>
2023-07-28 11:42:05 +0900

    Show area

--- app/src/main/java/com/sangwon/example/bookapp/SelectAreaActivity.kt
+++ app/src/main/java/com/sangwon/example/bookapp/SelectAreaActivity.kt
@@ -2,6 +2,7 @@ package com.sangwon.example.bookapp
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.opencsv.CSVReader
 import com.sangwon.example.bookapp.databinding.ActivitySelectAreaBinding
@@ -10,7 +11,17 @@ import java.io.InputStreamReader
 
 class SelectAreaActivity : AppCompatActivity() {
     lateinit var binding: ActivitySelectAreaBinding
-    lateinit var area:HashMap<String, HashMap<String, ArrayList<String>>>
+    lateinit var area: HashMap<String, HashMap<String, ArrayList<String>>>
+    lateinit var cityAdapter:ArrayAdapter<String>
+    val states =
+        "경기도, 대구광역시, 충청북도, 충청남도, 제주특별자치도, 강원도, 세종특별자치시, 울산광역시, 대전광역시, 광주광역시, 전라남도, 부산광역시, 전라북도, 경상남도, 서울특별시, 경상북도, 인천광역시".split(
+            ", "
+        )
+    private var cities = ArrayList<String>()
+    private lateinit var city:String
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySelectAreaBinding.inflate(layoutInflater)
@@ -18,19 +29,42 @@ class SelectAreaActivity : AppCompatActivity() {
 
         areaDatePreprocessing()
 
-//        val stateList = binding.state
-//        val cityList = binding.city
-//        val townList = binding.town
-//        stateList.selectedItem.
+
+        val stateAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, states)
+        val stateList = binding.state
+        val cityList = binding.city
+        val townList = binding.town
+        stateList.adapter = stateAdapter
+        stateList.setOnItemClickListener {_,_,position,_ ->
+            cities = ArrayList()
+            city = stateAdapter.getItem(position).toString()
+            for (city in area[stateAdapter.getItem(position)]?.keys!!) {
+                cities.add(city)
+            }
+            cityAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cities)
+            cityList.adapter = cityAdapter
+        }
+        cityList.setOnItemClickListener {_,_,position,_ ->
+            val towns = area[city]?.get(cities[position])?: ArrayList()
+            Log.d("selectedTown", towns.toString())
+            Log.d("selectedId", position.toString())
+
+/*            if (townss != null) {
+                for (town in townss) {
+                    towns.add(town)
+                }
+            }else{
+                towns.add("${cityAdapter.getItem(position)} 전체")
+            }*/
+            val townAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, towns)
+            townList.adapter = townAdapter
+        }
     }
 
-    fun areaDatePreprocessing() {
-        val states = "경기도, 대구광역시, 충청북도, 충청남도, 제주특별자치도, 강원도, 세종특별자치시, 울산광역시, 대전광역시, 광주광역시, 전라남도, 부산광역시, 전라북도, 경상남도, 서울특별시, 경상북도, 인천광역시".split(", ")
+    private fun areaDatePreprocessing() {
 
         val areaInput = baseContext.resources.assets.open("area_library.csv")
-
         val csvReader = CSVReader(InputStreamReader(areaInput, "EUC-KR"))
-
         val allContent = csvReader.readAll() as List<Array<String>>
 
         area = HashMap()
@@ -44,21 +78,18 @@ class SelectAreaActivity : AppCompatActivity() {
                 content[0] + " 도: " + content[1] + " 시: " + content[2] + " 동: " + content[3]
             )*/
             if (area.containsKey(content[1])) {
+                var town = ArrayList<String>()
                 if (area[content[1]]?.contains(content[2])!!) {
-                    val town = area[content[1]]?.get(content[2])
-                    town?.add(content[3])
-                    Log.d("citys",town.toString())
-                    area[content[1]]?.put(content[2], town!!)
-                }else{
-                    val town = ArrayList<String>()
-                    town.add(content[3])
-                    area[content[1]]?.put(content[2], town)
+                    town = area[content[1]]?.get(content[2])!!
                 }
+                town?.add(content[3])
+                area[content[1]]?.put(content[2], town!!)
+                Log.d("Allcitys", "state: ${content[1]} city: ${content[2]} towns: $town")
             }
         }
-        Log.d("states",area.keys.toString())
+        Log.d("states", area.toString())
         for (state in area.keys) {
-            Log.d("Allcitys",area[state].toString())
+            Log.d("Allcitys", area[state].toString())
         }
 //
 //        Log.d("towns",area.keys.toString())
