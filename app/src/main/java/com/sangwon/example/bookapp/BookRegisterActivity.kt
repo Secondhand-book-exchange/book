930e53aa3874168d7d6fce1cb871d6725079f3b0
entellaKa <inchol321@gmail.com>
2023-07-31 16:23:48 +0900

    Add status and category
    add spinner for status and radio buttons for category in BookRegisterActivity

--- app/src/main/java/com/sangwon/example/bookapp/BookRegisterActivity.kt
+++ app/src/main/java/com/sangwon/example/bookapp/BookRegisterActivity.kt
@@ -1,13 +1,15 @@
 package com.sangwon.example.bookapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import com.sangwon.example.bookapp.databinding.ActivityBookRegisterBinding
-import com.sangwon.example.bookapp.databinding.ActivityMainBinding
 
 class BookRegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBookRegisterBinding
@@ -24,38 +26,41 @@ class BookRegisterActivity : AppCompatActivity() {
             val BookTitle = binding.BookTitle.text.toString()
             val Author = binding.Author.text.toString()
             val Location = binding.Location.text.toString()
-            val BookStatus = binding.BookStatus.toString()
+            val BookStatus = binding.BookStatus.selectedItem.toString()
             val Subscript = binding.Subscript.text.toString()
             val IsSale = false
 
             val Post = Posts(
-                BookTitle,
-                Author,
-                Location,
-                BookStatus,
-                Subscript,
-                null,
-                IsSale,
-                null,
-                null,
+                    BookTitle,
+                    Author,
+                    Location,
+                    BookStatus,
+                    Subscript,
+                    null,
+                    IsSale,
+                    null,
+                    null,
             )
 
             db.collection("Posts")
-                .add(Post)
-                .addOnSuccessListener { documentReference ->
-                    Log.d("db", "DocumentSnapshot added with ID: ${documentReference.id}")
-                }
-                .addOnFailureListener { e ->
-                    Log.w("db", "Error adding document", e)
-                }
+                    .add(Post)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d("db", "DocumentSnapshot added with ID: ${documentReference.id}")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("db", "Error adding document", e)
+                    }
         }
 
+        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, resources.getStringArray(R.array.status))
+        binding.BookStatus.adapter = spinnerAdapter
+        binding.BookStatus.setSelection(0)
 
-
-
-
-
-
-
+        val rg = binding.category
+        for (v: String in resources.getStringArray(R.array.category)){
+            val rbtn = RadioButton(this)
+            rbtn.text = v
+            rg.addView(rbtn)
+        }
     }
 }
