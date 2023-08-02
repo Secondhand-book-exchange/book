069f12d60fa980eea9b8966437f486d81e15452d
Sangwon <qmfdnjssk@tukorea.ac.kr>
2023-08-02 11:21:52 +0900

    feat: 채팅 1차구현

---
+++ app/src/main/java/com/sangwon/example/bookapp/Adapter/MessageAdapter.kt
@@ -0,0 +1,73 @@
+package com.sangwon.example.bookapp.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.sangwon.example.bookapp.Message
+import com.sangwon.example.bookapp.R
+import org.w3c.dom.Text
+
+class MessageAdapter(private val context: Context, private val messageList : ArrayList<Message>):
+    RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    //뷰홀더 결정
+    private val receive = 1 //받는 타입
+    private val send = 2 //보내는 타입
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if(viewType == 1) { //받는 화면
+            val view: View = LayoutInflater.from(context).inflate(R.layout.receive, parent, false)
+            ReceiveViewHolder(view)
+        }else{ //보내는 화면
+            val view: View = LayoutInflater.from(context).inflate(R.layout.send, parent, false)
+            SendViewHolder(view)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return messageList.size
+    }
+
+    //여기서 반환된 값이 oncreateViewHolder의 viewType에 들어있습니다.
+    override fun getItemViewType(position: Int): Int {
+        //메시지값
+        val currentMessage = messageList[position]
+
+        return if(FirebaseAuth.getInstance().currentUser?.uid.equals(currentMessage.sendId)){
+            send
+        }else{
+            receive
+        }
+    }
+
+    //데이터 연결
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        //현재 메시지
+        val currentMessage = messageList[position]
+
+        //보내는 데이터
+        if(holder.javaClass == SendViewHolder::class.java){
+            val viewHolder = holder as SendViewHolder
+            viewHolder.sendMessage.text = currentMessage.message
+        }else{//받는 데이터
+            val viewHolder = holder as ReceiveViewHolder
+            viewHolder.receiveMessage.text = currentMessage.message
+        }
+    }
+
+    //보낸 쪽
+    class SendViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val sendMessage: TextView = itemView.findViewById(R.id.send_message_text)
+    }
+
+    //받는 쪽
+    class ReceiveViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val receiveMessage: TextView = itemView.findViewById(R.id.receive_message_text)
+    }
+
+
+
+}
--- app/src/main/java/com/sangwon/example/bookapp/ChatActivity.kt
+++ app/src/main/java/com/sangwon/example/bookapp/ChatActivity.kt
@@ -3,10 +3,12 @@ package com.sangwon.example.bookapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.sangwon.example.bookapp.Adapter.MessageAdapter
 import com.sangwon.example.bookapp.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity() {
@@ -18,10 +20,20 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var receiverRoom: String //받는 대화방
     private lateinit var senderRoom: String //보낸 대화방
 
+    private lateinit var messageList: ArrayList<Message>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //초기화
+        messageList = ArrayList()
+        val messageAdapter: MessageAdapter = MessageAdapter(this, messageList)
+
+        //RecyclerView
+        binding.chatRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.chatRecyclerView.adapter = messageAdapter
 
         //넘어온 데이터 변수에 담기
 //        receiverName = intent.getStringExtra("name").toString()
@@ -47,16 +59,47 @@ class ChatActivity : AppCompatActivity() {
 
         //메시지 전송 버튼 이벤트
         binding.sendButton.setOnClickListener{
-            Log.e("BBB","Clicked")
             val message = binding.messageEdit.text.toString()
             val messageObject = Message(message, senderUid)
+
+            // 데이터 저장
             db.collection("Chats").document(senderUid!!)
                 .collection("${senderRoom}").document().set(messageObject)
                 .addOnSuccessListener {
                     db.collection("Chats").document(receiverUid)
                         .collection("${receiverRoom}").document().set(messageObject)
                 }
+            //입력값 초기화
+            binding.messageEdit.setText("")
         }
 
+        //메시지 가져오기
+        db.collection("Chats").document(senderUid!!)
+            .collection("${senderRoom}")
+            .addSnapshotListener { snapshot, e ->
+                if (snapshot != null && !snapshot.isEmpty) {
+                   messageList.clear()
+
+                    for(document in snapshot.documents){
+                        val message = document.getString("message")
+                        val senderUid = document.getString("senderUid")
+
+                        val messageObject = Message(message,senderUid)
+                        messageList.add(messageObject)
+
+                    }
+                    messageAdapter.notifyDataSetChanged()
+
+                } else {
+                    Log.e("snapshoterror", "Error getting messages: $e")
+                }
+            }
+
+
+
+
     }
+
+
+
 }
