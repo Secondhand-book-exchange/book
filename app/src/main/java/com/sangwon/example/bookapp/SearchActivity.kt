817e92fc3cc8b144bfb0076aff17afb03fc698c5
entellaKa <inchol321@gmail.com>
2023-08-08 14:57:41 +0900

    Record search keyword(-ing)

--- app/src/main/java/com/sangwon/example/bookapp/SearchActivity.kt
+++ app/src/main/java/com/sangwon/example/bookapp/SearchActivity.kt
@@ -1,19 +1,51 @@
 package com.sangwon.example.bookapp
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import com.sangwon.example.bookapp.Adapter.BookListAdapter
+import androidx.core.content.edit
 import com.sangwon.example.bookapp.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var binding:ActivitySearchBinding
-    private lateinit var list:BookListFragment
+    private lateinit var binding: ActivitySearchBinding
+    private lateinit var list: BookListFragment
+
+    private lateinit var searchRecord: SharedPreferences
+    private lateinit var searchAdapter:ArrayAdapter<String>
+    private var recordSet = setOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        searchRecord = getSharedPreferences("searchRecord", Context.MODE_PRIVATE)
+        recordSet = searchRecord.getStringSet("record", setOf<String>()) ?: setOf()
+
+        searchAdapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, recordSet.toTypedArray())
+        binding.searchRecord.adapter = searchAdapter
+        binding.searchRecord.setOnItemClickListener { _, _, pos, _ ->
+            val keyword = searchAdapter.getItem(pos)
+            binding.searchText.setText(keyword)
+            binding.searchBtn.callOnClick()
+        }
+        binding.searchRecord.setOnItemLongClickListener { _, _, _, _ ->
+            false
+        }
+
+        binding.searchText.setOnEditorActionListener { v, actionId, event ->
+            when (actionId) {
+                EditorInfo.IME_ACTION_SEARCH -> {
+                    binding.searchBtn.callOnClick()
+                }
+            }
+            true
+        }
         binding.searchBtn.setOnClickListener(this)
     }
 
@@ -24,6 +56,12 @@ class SearchActivity : AppCompatActivity(), View.OnClickListener {
                 list = BookListFragment("search")
                 list.setKeyword(searchKeyWord)
                 supportFragmentManager.beginTransaction().replace(binding.list.id, list).commit()
+                searchAdapter.add(searchKeyWord)
+                recordSet.plus(searchKeyWord)
+                searchRecord.edit {
+                    putStringSet("record", recordSet)
+                    apply()
+                }
             }
         }
     }
