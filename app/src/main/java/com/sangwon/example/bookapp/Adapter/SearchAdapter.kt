4c3767d03712783c6b1c83caf697604077b3736b
entellaKa <inchol321@gmail.com>
2023-08-08 15:47:39 +0900

    add search record
    search

---
+++ app/src/main/java/com/sangwon/example/bookapp/Adapter/SearchAdapter.kt
@@ -0,0 +1,49 @@
+package com.sangwon.example.bookapp.Adapter
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.sangwon.example.bookapp.R
+
+class SearchAdapter : BaseAdapter() {
+    private var items = ArrayList<String>()
+    private  var p = -1
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return items[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val context = parent?.context
+        val view = if (convertView != null) convertView
+        else {
+            val inflate: LayoutInflater =
+                context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            inflate.inflate(R.layout.area_item, parent,false)
+        }
+
+        if (position == p)
+            view.setBackgroundColor(Color.parseColor("#D0F5BE"))
+        else
+            view.setBackgroundColor(Color.parseColor("#FFFFFF"))
+
+        view.findViewById<TextView>(R.id.area).text = items[position]
+
+        return view
+    }
+
+    fun add(areas: String) {
+        items.add(areas)
+    }
+}
--- app/src/main/java/com/sangwon/example/bookapp/SearchActivity.kt
+++ app/src/main/java/com/sangwon/example/bookapp/SearchActivity.kt
@@ -3,11 +3,13 @@ package com.sangwon.example.bookapp
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
+import androidx.core.view.isGone
+import com.sangwon.example.bookapp.Adapter.SearchAdapter
 import com.sangwon.example.bookapp.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity(), View.OnClickListener {
@@ -15,8 +17,8 @@ class SearchActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var list: BookListFragment
 
     private lateinit var searchRecord: SharedPreferences
-    private lateinit var searchAdapter:ArrayAdapter<String>
-    private var recordSet = setOf<String>()
+    private lateinit var searchAdapter:SearchAdapter
+    private var recordSet = mutableSetOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,20 +26,30 @@ class SearchActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
 
         searchRecord = getSharedPreferences("searchRecord", Context.MODE_PRIVATE)
-        recordSet = searchRecord.getStringSet("record", setOf<String>()) ?: setOf()
+        recordSet = searchRecord.getStringSet("record", setOf<String>()) ?: mutableSetOf()
 
-        searchAdapter =
-            ArrayAdapter(this, android.R.layout.simple_list_item_1, recordSet.toTypedArray())
+        searchAdapter = SearchAdapter()
+        for (keyword in recordSet){
+            searchAdapter.add(keyword)
+        }
         binding.searchRecord.adapter = searchAdapter
         binding.searchRecord.setOnItemClickListener { _, _, pos, _ ->
-            val keyword = searchAdapter.getItem(pos)
+            val keyword = searchAdapter.getItem(pos) as String
             binding.searchText.setText(keyword)
             binding.searchBtn.callOnClick()
         }
         binding.searchRecord.setOnItemLongClickListener { _, _, _, _ ->
             false
         }
-
+        binding.searchText.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                binding.searchRecord.isGone = false
+                binding.list.isGone = true
+            }else{
+                binding.list.isGone = false
+                binding.searchRecord.isGone = true
+            }
+        }
         binding.searchText.setOnEditorActionListener { v, actionId, event ->
             when (actionId) {
                 EditorInfo.IME_ACTION_SEARCH -> {
@@ -56,8 +68,11 @@ class SearchActivity : AppCompatActivity(), View.OnClickListener {
                 list = BookListFragment("search")
                 list.setKeyword(searchKeyWord)
                 supportFragmentManager.beginTransaction().replace(binding.list.id, list).commit()
+                binding.searchText.clearFocus()
+
                 searchAdapter.add(searchKeyWord)
-                recordSet.plus(searchKeyWord)
+                recordSet.add(searchKeyWord)
+                Log.e("keywords", "$recordSet")
                 searchRecord.edit {
                     putStringSet("record", recordSet)
                     apply()
